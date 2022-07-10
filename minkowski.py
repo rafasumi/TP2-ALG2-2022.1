ed7edab4f542eefc5948b9fc6657c7c74ce792ca
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2022-07-10 20:48:58 -0300

    Atualizando minkowski

--- minkowski.py
+++ minkowski.py
@@ -1,4 +1,7 @@
 import numpy as np
 
 def minkowski_dist(X, Y, p=1):
-  return (np.sum((abs(X - Y))**p)) ** (1/p)
+  return (np.sum((abs(X - Y)**p))) ** (1/p)
+
+def get_dist_matrix(data):
+  return np.asarray([list(map(lambda X: minkowski_dist(X, point), data)) for point in data])
